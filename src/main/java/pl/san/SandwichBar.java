eb7591620e0b611d28b791205b97b3a65304aee1
Krystian Guminski <krystian.guminsk@gmail.com>
2020-05-30 21:12:21 +0200

    Task 6

--- src/main/java/pl/san/SandwichBar.java
+++ src/main/java/pl/san/SandwichBar.java
@@ -8,6 +8,10 @@ import pl.san.model.ingredient.*;
 import pl.san.sandwich.Sandwich;
 import pl.san.sandwich.SandwichMaker;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class SandwichBar {
 
     public static void main(String[] args) {
@@ -31,5 +35,27 @@ public class SandwichBar {
         sandwichBox.sandwichInfo();
         sandwichBox.paymentMethod(PAYMENT.CASH);
         System.out.println("\n"+sandwichBox);
+
+        //Task 6
+        System.out.println("\nTask 6");
+
+        List<Double> doubles = new ArrayList<>();
+        var numbersOfElements = ThreadLocalRandom.current().nextInt(20) + 10;
+
+        for (int i = 0; i < numbersOfElements; i++){
+            doubles.add(ThreadLocalRandom.current().nextDouble()*100);
+        }
+
+        doubles.sort(Double::compareTo);
+        doubles.forEach(System.out::println);
+
+        var avg  = doubles.stream().reduce(Double::sum).get() / doubles.size();
+
+        var median = switch (doubles.size() % 2){
+            case 0 -> doubles.get((doubles.size()/2)) + doubles.get((doubles.size()/2 - 1)) / 2.0d;
+            default -> doubles.get(doubles.size() / 2);
+        };
+
+        System.out.println("Elements = " + doubles.size() + "\nAVG = " + avg + "\nMedian = " + median);
     }
 }
